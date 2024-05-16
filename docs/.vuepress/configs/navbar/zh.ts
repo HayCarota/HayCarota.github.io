311bcffaa73fe102c0ae6b800b15374b1d657ca8
onlyMrz <1914810765@qq.com>
2024-05-17 01:08:12 +0800

    chore: update navbar

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -12,45 +12,45 @@ export const zh: NavbarConfig = [
     link: "/tags/",
     icon: "fa-tag"
   },
-  {
-    text: "链接",
-    link: "/links/",
-    icon: "fa-satellite-dish"
-  },
+  // {
+  //   text: "链接",
+  //   link: "/links/",
+  //   icon: "fa-satellite-dish"
+  // },
   {
     text: "文档",
     link: "/zh/docs/basic/intro.md",
     icon: "ri-book-2-fill"
   },
-  {
-    text: "VuePress",
-    link: "https://v2.vuepress.vuejs.org/zh/",
-    icon: "ri-vuejs-line"
-  },
-  {
-    text: `v${version}`,
-    icon: "co-git",
-    children: [
-      {
-        text: "GitHub",
-        link: "https://github.com/Renovamen/vuepress-theme-gungnir",
-        icon: "ri-github-line"
-      },
-      {
-        text: "更新日志",
-        link: "https://github.com/Renovamen/vuepress-theme-gungnir/blob/main/CHANGELOG.md",
-        icon: "oi-git-compare"
-      },
-      {
-        text: "v2.x",
-        link: "/zh/docs/intro.md",
-        icon: "oi-rocket"
-      },
-      {
-        text: "v0.x",
-        link: "https://vuepress-theme-gungnir.vercel.app",
-        icon: "ri-sailboat-line"
-      }
-    ]
-  }
+  // {
+  //   text: "VuePress",
+  //   link: "https://v2.vuepress.vuejs.org/zh/",
+  //   icon: "ri-vuejs-line"
+  // },
+  // {
+  //   text: `v${version}`,
+  //   icon: "co-git",
+  //   children: [
+  //     {
+  //       text: "GitHub",
+  //       link: "https://github.com/Renovamen/vuepress-theme-gungnir",
+  //       icon: "ri-github-line"
+  //     },
+  //     {
+  //       text: "更新日志",
+  //       link: "https://github.com/Renovamen/vuepress-theme-gungnir/blob/main/CHANGELOG.md",
+  //       icon: "oi-git-compare"
+  //     },
+  //     {
+  //       text: "v2.x",
+  //       link: "/zh/docs/intro.md",
+  //       icon: "oi-rocket"
+  //     },
+  //     {
+  //       text: "v0.x",
+  //       link: "https://vuepress-theme-gungnir.vercel.app",
+  //       icon: "ri-sailboat-line"
+  //     }
+  //   ]
+  // }
 ];
